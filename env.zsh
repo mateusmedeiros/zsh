a02a864e8b0ccbae5f2ddcc6128f5a6b4c4410f4
Mateus "Doodad" Medeiros <mateusbmedeiros@outlook.com>
2017-02-18 22:19:10 +0000

    Organize $PATH modifications a little better

--- env.zsh
+++ env.zsh
@@ -4,18 +4,8 @@ export EDITOR="nvim"
 # used by certain java applications
 export JAVA_HOME="/usr/lib/jvm/java-8-jdk"
 
-# path modifications
-export PATH="$HOME/.rvm/bin:$PATH"
-export PATH="$HOME/gradle-2.5/bin:$PATH"
-export PATH="/usr/local/sbin:$PATH"
-export PATH="/usr/local/bin:$PATH"
-export PATH="/usr/local/heroku/bin:$PATH"
-export PATH="/usr/bin:$PATH"
-export PATH="/usr/bin/core_perl:$PATH"
-export PATH="${HOME}/.cargo/bin:$PATH"
-
 # cdpath modifications
-# basically PATH for cd
+# basically $PATH for cd
 # more info on http://zsh.sourceforge.net/Doc/Release/Parameters.html
 export CDPATH="$HOME/workspace"
 export CDPATH="$HOME/workspace/saites:$CDPATH"
@@ -23,6 +13,15 @@ export CDPATH="$HOME/workspace/eclipse:$CDPATH"
 export CDPATH="$HOME/workspace/ecostage:$CDPATH"
 export CDPATH="$HOME/workspace/rust:$CDPATH"
 export CDPATH="$HOME/.config:$CDPATH"
+
+# gradle
+export PATH="$HOME/gradle-2.5/bin:$PATH"
+
+# heroku
+export PATH="/usr/local/heroku/bin:$PATH"
+
+# perl
+export PATH="/usr/bin/core_perl:$PATH"
 
 # asdf (Elixir and Erlang version manager)
 source ${XDG_CONFIG_HOME:-$HOME/.config}/asdf/asdf.sh
